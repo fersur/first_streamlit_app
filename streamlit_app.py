91362be6eee060c6e88b60e15c4ef7a800b63e3c
fersur <haoprofile@gmail.com>
2024-03-12 21:49:44 -0500

    Update streamlit_app.py
    ADding text_input and confirmation message

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,10 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.header("Fruityvice Fruit Advice!! second line")
+
+# adding text field and notify user
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -41,6 +44,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # put the normalized result into data frame
 streamlit.dataframe(fruityvice_normalized)
 
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -50,3 +55,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruity list contains: ")
 streamlit.dataframe(my_data_row)
 
+#allow user to add fruit to the list
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('The user entered ', fruit_choice2)
