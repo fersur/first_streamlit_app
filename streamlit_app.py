39fc409c9eab0e63561efe6fad01354ef1c43ccd
fersur <haoprofile@gmail.com>
2024-03-12 22:43:55 -0500

    Update streamlit_app.py
    Replace insert_fruit to accept variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def get_fruit_list():
 
 def insert_new_fruit(new_fruit):
     with my_cnx.cursor() as my_cur:
-            my_cur.execute("insert into fruit_load_list values('from_streamlit too')")
+            my_cur.execute("insert into fruit_load_list values('" + new_fruit + "')")
             return "Thanks for adding " +  new_fruit + " to the list."
 
 try:  
